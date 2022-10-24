a3818414e7fc07dd654e0512c7f832b2e8c387c1
YanNurkov <85751034+YanNurkov@users.noreply.github.com>
2022-10-24 17:06:33 +0300

    Initial Commit

---
+++ Alef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alef
+//
+//  Created by Ян Нурков on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
